2566305365d41080a1f9c4accf5ced1471c96c12
Larissa Nobrega <105369910+larissanobregaa@users.noreply.github.com>
2024-10-01 13:39:20 -0300

    Add files via upload

---
+++ bigdata-certo/bigdata/src/main/java/br/edu/ibmec/bigdata/controller/EnderecoController.java
@@ -0,0 +1,43 @@
+package br.edu.ibmec.bigdata.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ibmec.bigdata.model.Endereco;
+import br.edu.ibmec.bigdata.service.EnderecoService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/enderecos")
+public class EnderecoController {
+    private final EnderecoService enderecoService;
+    
+    public EnderecoController(EnderecoService enderecoService) {
+        this.enderecoService = enderecoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Endereco> adicionarEndereco(@Valid @RequestBody Endereco endereco) {
+        Endereco novoEndereco = enderecoService.adicionarEndereco(endereco);
+        return ResponseEntity.ok(novoEndereco);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Endereco> atualizarEndereco(@PathVariable int id, @org.jetbrains.annotations.NotNull @Valid @RequestBody Endereco endereco){
+        endereco.setId(id);
+        Endereco enderecoAtualizado = enderecoService.atualizarEndereco(endereco);
+        return ResponseEntity.ok(enderecoAtualizado);
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> removerEndereco(@PathVariable int id){
+        enderecoService.removerEndereco(id);
+        return ResponseEntity.noContent().build();
+    }
+}
